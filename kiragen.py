46610b4227163eaf3b7a6ff483d2bcd935ce81d0
DehaTuzcu-31 <karahanozlem470776@gmail.com>
2025-05-04 15:02:21 +0300

    Update kiragen.py

--- kiragen.py
+++ kiragen.py
@@ -1,30 +1,19 @@
 import random
 
-def generate_visa_credit_card(num_cards, pause_each=False):
-    cards = []
-    for i in range(1, num_cards + 1):
-        card_number = "4" + "".join([str(random.randint(0, 9)) for _ in range(15)])
-        expiration_year = random.randint(2025, 2031)
-        expiration_month = str(random.randint(1, 12)).zfill(2)
-        expiration_date = f"{expiration_month}/{expiration_year}"
-        cvv = str(random.randint(100, 999))
-        card = f"[ID{i}]|{card_number}|{expiration_date}|{cvv}"
-        cards.append(card)
-        print(card)
-        if pause_each:
-            input("Devam etmek için Enter'a basın...")
-            print()
-    return cards
+def generate_visa_card():
+    # Kart numarası: 16 haneli, 4 ile başlıyor
+    card_number = '4' + ''.join(str(random.randint(0, 9)) for _ in range(15))
 
-# Kullanıcıdan kart sayısını alın
-num_cards = int(input("Kaç tane Visa kredi kartı oluşturmak istiyorsunuz? "))
+    # Son kullanma tarihi: Ay (01-12) ve gelecek yıllar (2025-2030 arası)
+    exp_month = f"{random.randint(1, 12):02}"
+    exp_year = str(random.randint(2025, 2030))
 
-# Kartları oluşturun
-cards = generate_visa_credit_card(num_cards, pause_each=False)
+    # CVV: 3 haneli
+    cvv = ''.join(str(random.randint(0, 9)) for _ in range(3))
 
-# Kartları yazdırın
-print("\n--- Oluşturulan Kartlar ---")
-for card in cards:
-    print(card)
+    return f"{card_number}|{exp_month}|{exp_year}|{cvv}"
 
-input("\nÇıkmak için Enter'a basın.")
+# Örnek olarak 5 sahte kart üretelim
+for _ in range(5):
+    print(generate_visa_card())
+input()
